d3dd7f7c065a587371cbdb7a14438ac54e93e6aa
herryho <herry.heyi@gmail.com>
2022-06-30 17:37:45 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// 导入dotenv模块,获取环境变量
+require("dotenv").config();
+const FILECOIN_PROJECT_ID = process.env.FILECOIN_PROJECT_ID;
+const FILECOIN_PROJECT_SECRET = process.env.FILECOIN_PROJECT_SECRET;
+const WSS_ENDPOINT = process.env.WSS_ENDPOINT;
+
+const main = async () => {};
+
+main();
